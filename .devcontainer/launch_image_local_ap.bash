d42dcb3f60a95a35ac1e0100c3817cd8af4571bc
alicepiller <alicerpiller@gmail.com>
2025-03-07 10:25:43 +0000

    New docker container test file for local use

---
+++ .devcontainer/launch_image_local_ap.bash
@@ -0,0 +1,3 @@
+LOCAL_DATA_PATH="C:\Users\Alice.Piller\OneDrive - AHRI\Documents\_M.Sc. Health Data Science\Course work\Term 2\AIHGP - Artificial Intelligence in Healthcare Group Project\Blender"
+docker run -v ${LOCAL_DATA_PATH}:/blender_colonoscopy/datasets/blend_files/ --name blender_colonoscopy_container --detach blender_colonoscopy:latest
+docker exec -it blender_colonoscopy_container bash
